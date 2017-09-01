f1e8c7e5a1c8524c7ee882f0ed16ed368adde5fe
Gabriele N. Tornetta <phoenix1987@gmail.com>
2017-09-01 15:56:43 +0200

    Showing all the gravity sensors detected by programmatically adding new TextView elements to the LinearLayout container

--- src/main/java/com/p403n1x87/androtest/MainActivity.java
+++ src/main/java/com/p403n1x87/androtest/MainActivity.java
@@ -10,65 +10,73 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
+import android.view.ViewGroup.LayoutParams;
+
 import android.widget.TextView;
+import android.widget.LinearLayout;
 
 import java.util.List;
 
 import static java.lang.Math.sqrt;
 
 
-public class MainActivity extends Activity implements SensorEventListener
+public class MainActivity extends Activity
 {
-
   private SensorManager mSensorManager;
   private Sensor        mSensor;
 
-  private TextView      text = null;
+  private TextView      text       = null;
+  private LinearLayout  layoutMain = null;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+    // UI
 		setContentView(R.layout.main_layout);
-    if (text == null) text = (TextView) findViewById(R.id.text);
+    if (text == null)       text       = (TextView)     findViewById(R.id.text);
+    if (layoutMain == null) layoutMain = (LinearLayout) findViewById(R.id.layout_main);
 
+    // Sensors
     mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
     if (mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY) != null){
       List<Sensor> gravSensors = mSensorManager.getSensorList(Sensor.TYPE_GRAVITY);
-      Log.i("AndroTest", "There are " + Integer.toString(gravSensors.size()) + " grav sensors");
-      mSensor = gravSensors.get(0);
-      text.setText("We have gravity! :D :D :D");
-      mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+      int nSensors = gravSensors.size();
+      text.setText("Detected gravity sensors: " + Integer.toString(nSensors));
+
+      for (int i = 0; i < nSensors; i++) {
+        final TextView tvSensor = new TextView(this);
+        final int      j        = i + 1;
+
+        tvSensor.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+
+        mSensor = gravSensors.get(i);
+        mSensorManager.registerListener(new SensorEventListener() {
+          @Override
+          public void onSensorChanged(final SensorEvent event) {
+            runOnUiThread(new Runnable() {
+              @Override
+              public void run() {
+        		    float x = event.values[0];
+        		    float y = event.values[1];
+        		    float z = event.values[2];
+        		    float g = (float) sqrt(x*x + y*y + z*z);
+
+                tvSensor.setText(String.format("Sensor %d: %f m/s^2", j, g));
+              }
+            });
+          }
+
+          @Override
+          public void onAccuracyChanged(Sensor sensor, int a) {}
+        }, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        layoutMain.addView(tvSensor);
+      }
     }
     else {
       text.setText("We DO NOT have gravity! :(");
     }
 	}
-
-
-  /************************************************************************************************
-   * Gravity Sensor Callbacks
-   ************************************************************************************************/
-
-  @Override
-  public void onSensorChanged(final SensorEvent event) {
-		runOnUiThread(new Runnable() {
-      @Override
-      public void run() {
-		    float x = event.values[0];
-		    float y = event.values[1];
-		    float z = event.values[2];
-		    float g = (float) sqrt(x*x + y*y + z*z);
-
-        text.setText("Gravity: " + Float.toString(g) + " meters per second per second");
-      }
-    });
-  }
-
-  @Override
-  public final void onAccuracyChanged(Sensor sensor, int accuracy) {
-    // Do something here if sensor accuracy changes.
-  }
-
 
 }
